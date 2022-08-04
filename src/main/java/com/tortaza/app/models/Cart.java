68fb07f95fa9d1b2d77ff6ec7ca482317a0da814
nelson <nelsonc897@gmail.com>
2022-08-03 19:00:08 -0500

    cart basic

---
+++ src/main/java/com/tortaza/app/models/Cart.java
@@ -0,0 +1,19 @@
+package com.tortaza.app.models;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer carritoId;
+
+    
+
+
+
+
+}
